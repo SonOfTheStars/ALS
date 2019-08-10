8e931e28d0e61e3aef48184d0284b1328561ec0c
Bitterholz <simon.wienecke94@googlemail.com>
2019-08-10 19:25:45 +0200

    Initial Commit

---
+++ src/main/java/com/sots/als/AdvancedLogisticsSystems.java
@@ -0,0 +1,7 @@
+package com.sots.als;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(  modid = "als", name = "AdvancedLogisticsSystems", version = "0.1_INDEV", useMetadata = true, dependencies = "jei")
+public class AdvancedLogisticsSystems {
+}
